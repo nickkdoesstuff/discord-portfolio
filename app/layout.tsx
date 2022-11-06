a53de7755cbfacc52a19d53ed4fd5c2635f70935
nickkdoesstuff <69115738+nickkdoesstuff@users.noreply.github.com>
2022-11-06 10:17:25 +0000

    removed cursor

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,7 @@ export default function RootLayout({
       <head>
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
       </head>
-      <body className="bg-zinc-900 flex flex-col gap-2 items-center justify-center h-[100vh] text-white cursor-none">
-        <Cursor />
+      <body className="bg-zinc-900 flex flex-col gap-2 items-center justify-center h-[100vh] text-white">
         <div className="bg-zinc-800 p-2 rounded-lg border-solid border-2 border-zinc-700">
           <div className="flex gap-3">
             <Link className="material-symbols-rounded" href="/">home</Link>
